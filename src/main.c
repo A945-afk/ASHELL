b37fcdc75705b4b992fc8c5be95a5aee1cb267e2
A945-afk <arnabmondal6942088@gmail.com>
2026-02-08 18:32:36 +0600

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -3,6 +3,12 @@
 #include <string.h>
 
 #define MAX_INPUT 512
+
+char* builtin[] = {"exit","echo","type"};
+int built_count = 3;
+
+
+
 int main(int argc, char *argv[]) 
 {
   while(1)
@@ -14,10 +20,30 @@ int main(int argc, char *argv[])
     fgets(ibuf, MAX_INPUT, stdin);
     ibuf[strcspn(ibuf, "\n")] = '\0';
     if (!strcasecmp(ibuf, "exit")) break;
-    if(!strncasecmp(ibuf, "echo ", 5))
+    if(!strncasecmp(ibuf, builtin[1], 5))
     {
       char* echo_out = ibuf + 5;
       printf("%s\n",echo_out);
+      continue;
+    }
+    else if(!strncasecmp(ibuf, builtin[2], 5))
+    {
+      char* type_out = ibuf + 5;
+      int type_match = 0;
+      for (int i = 0; i < built_count; i++)
+      {
+        if (!strcasecmp(builtin[i], type_out))
+        {
+          printf("%s is a shell builtin",type_out);
+          type_match = 1;
+          break;
+        }
+        
+      }
+      if (!type_match)
+      {
+        printf("%s: not found", type_match);
+      }
       continue;
     }
     printf("%s: command not found\n", ibuf);
