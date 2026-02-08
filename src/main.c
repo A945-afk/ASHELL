a6767e7262cfc3568bc7e237b8b92cd44ebf129a
A945-afk <arnabmondal6942088@gmail.com>
2026-02-08 17:29:54 +0600

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(int argc, char *argv[]) {
+#define MAX_INPUT 512
+int main(int argc, char *argv[]) 
+{
   // Flush after every printf
   setbuf(stdout, NULL);
-
-  // TODO: Uncomment the code below to pass the first stage
-  printf("$ ");
+  char ibuf[MAX_INPUT];
+  fgets(ibuf, MAX_INPUT, stdin);
+  ibuf[strcspn(ibuf, "\n")] = '\0';
+  printf("%s: command not found\n", ibuf);
 
   return 0;
 }
