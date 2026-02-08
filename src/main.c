2d7495b8a3179a60a958fd68990687574f128c87
A945-afk <arnabmondal6942088@gmail.com>
2026-02-08 17:40:49 +0600

    codecrafters submit [skip ci]

--- src/main.c
+++ src/main.c
@@ -13,6 +13,11 @@ int main(int argc, char *argv[])
     char ibuf[MAX_INPUT];
     fgets(ibuf, MAX_INPUT, stdin);
     ibuf[strcspn(ibuf, "\n")] = '\0';
+    if (!strcasecmp(ibuf, "exit"))
+    {
+      break;
+    }
+    
     printf("%s: command not found\n", ibuf);
   }
   return 0;
